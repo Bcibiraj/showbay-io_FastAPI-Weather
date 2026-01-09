86124a7d5479117638189eadae63c37345cd838b
Bcibiraj <cibirajb20@gmail.com>
2026-01-09 19:14:55 +0530

    third commit

--- app/database.py
+++ app/database.py
@@ -10,9 +10,3 @@ SessionLocal = sessionmaker(bind=engine, autoflush=False,autocommit=False)
 Base = declarative_base()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
